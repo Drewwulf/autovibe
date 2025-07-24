95e7b14c5607f03514256751be787d7c0866bc4c
Dima <coninecd@gmail.com>
2025-07-24 19:03:37 +0300

    fix 404 on Netlify with _redirects

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,10 @@
 import React, { useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom'; // додаємо useNavigate для редіректу
-import { useAuth } from '../context/AuthContext'; // імпорт контексту авторизації
+import { NavLink } from 'react-router-dom'; // додаємо useNavigate для редіректу
 import './Header.css';
 
 function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
-    const { user, logout } = useAuth();  // отримуємо користувача і функцію виходу
-    const navigate = useNavigate();
 
-    async function handleLogout() {
-        try {
-            await logout();
-            navigate('/login'); // редірект після виходу
-        } catch (error) {
-            console.error("Помилка виходу:", error);
-        }
-    }
 
     return (
         <header className="header">
@@ -36,33 +25,6 @@ function Header() {
                         Контакти
                     </NavLink>
 
-
-                    {!user && (
-                        <>
-                            <NavLink to="/login" className="nav-link" onClick={() => setMenuOpen(false)}>Вхід</NavLink>
-                            <NavLink to="/register" className="nav-link" onClick={() => setMenuOpen(false)}>Реєстрація</NavLink>
-                        </>
-                    )}
-
-                    {user && (
-                        <div className="nav-user-info" style={{ color: '#fff', display: 'flex', alignItems: 'center', gap: '1rem', paddingLeft: '1rem' }}>
-                            <span>Привіт, {user.email}</span>
-                            <button
-                                onClick={handleLogout}
-                                style={{
-                                    backgroundColor: '#d42c2c',
-                                    border: 'none',
-                                    padding: '6px 12px',
-                                    borderRadius: '4px',
-                                    color: 'white',
-                                    cursor: 'pointer',
-                                    fontWeight: '700',
-                                }}
-                            >
-                                Вийти
-                            </button>
-                        </div>
-                    )}
                 </nav>
 
                 <div
